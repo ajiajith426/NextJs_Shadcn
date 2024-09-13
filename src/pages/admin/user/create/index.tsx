031fe389ae13e00d9f23312d2ca865ed6cfdfcd9
ajiajith426 <ajith.m@capecomsolutions.com>
2024-09-13 15:30:59 +0530

    Form

--- src/pages/admin/user/create/index.tsx
+++ src/pages/admin/user/create/index.tsx
@@ -1,7 +1,26 @@
 import React from "react";
+import {Breadcrumbs} from "@/components/shared-components/breadcrumbs";
+import UserFormComponent from "../userForm";
 
 function CreateUser() {
-  return <div>CreateUser</div>;
+  return (
+    <div>
+      <Breadcrumbs
+        items={[
+          {title: "Dashboard", link: "/admin/dashboard"},
+          {title: "Users", link: "/admin/user"},
+          {title: "Create", link: "/admin/user/create"},
+        ]}
+      />
+      <div className="space-y-2 py-10 text-center">
+        <h2 className="text-xl font-bold tracking-tight text-white sm:text-3xl">
+          Create User
+        </h2>
+        <p className="text-sm text-muted-foreground">User form creation</p>
+      </div>
+      <UserFormComponent />
+    </div>
+  );
 }
 
 export default CreateUser;
--- src/sections/user/user-table/index.tsx
+++ src/sections/user/user-table/index.tsx
@@ -2,6 +2,7 @@ import DataTableComponent from "@/components/shared-components/data-table";
 import TableSearchInput from "@/components/shared-components/table-search-input";
 import {Button} from "@/components/ui/button";
 import {Plus} from "lucide-react";
+import {useRouter} from "next/router";
 
 type UserTableProps = {
   users?: any;
@@ -11,6 +12,7 @@ type UserTableProps = {
 };
 
 export default function UsersTable({users, pageCount}: UserTableProps) {
+  const router = useRouter();
   return (
     <>
       <div className="flex items-center justify-between gap-2 py-5">
@@ -18,7 +20,10 @@ export default function UsersTable({users, pageCount}: UserTableProps) {
           <TableSearchInput />
         </div>
         <div className="flex gap-3">
-          <Button className="text-xs md:text-sm bg-white text-black hover:text-white">
+          <Button
+            className="text-xs md:text-sm bg-white text-black hover:text-white"
+            onClick={() => router.push("/admin/user/create")}
+          >
             <Plus className="mr-2 h-4 w-4" /> Add New
           </Button>
         </div>
